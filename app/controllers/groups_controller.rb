8aa47b1d3c7e17cc6264355b3460bb66d2272e98
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-01 10:50:59 +0800

    implement group#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,17 @@ class GroupsController < ApplicationController
   def index
     @group = Group.all
   end
+  def new
+    @group = Group.new
+  end
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to groups_path
+  end
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
