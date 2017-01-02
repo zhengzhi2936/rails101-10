35b25e1b6360fe856961b9f4d07213d77cc74179
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-02 14:52:58 +0800

    refix edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
     @group = Group.find(params[:group_id])
     @post = Post.new
   end
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -17,6 +21,24 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+    def destroy
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to account_posts_path, alert: "post deleted"
+    end
   private
   def post_params
     params.require(:post).permit(:content)
